8e0ac8cd53f43d78780a4aac702fd67f3e66a90b
Johnathan Thompson <jettt012@gmail.com>
2021-06-25 18:16:09 -0400

    Added what needs to be accomplished

--- Assignment06/Assignment06.cpp
+++ Assignment06/Assignment06.cpp
@@ -13,23 +13,29 @@ queue <int> q;
 int main()
 {
     int tally = 0;
-    // Make sure producer doesn't try to add to full queue and consumer doesen't try to remove from an empty queue
+    thread Prod1(&Producer::createNum, q, 1);
+    thread Prod2(&Producer::createNum, q, 2);
+    thread Con1(&Consumer::removeItem, q, 1);
+    thread Con2(&Consumer::removeItem, q, 2);
+    thread Con3(&Consumer::removeItem, q, 3);
     
-    
-    thread Con1(&Consumer::removeItem,q, 1);
-    thread Con2(&Consumer::removeItem,q, 2);
-    thread Prod1(&Producer::createNum,q, 1);
-    thread Prod2(&Producer::createNum,q, 2);
-    thread Prod3(&Producer::createNum,q, 3);
-
-
-    Con1.join();
-    Con2.join();
     Prod1.join();
     Prod2.join();
-    Prod3.join();
-
-    // Use sleep_until to have consumers to wait until producer produces item as well as sleeping when the queue is full
+    Con1.join();
+    Con2.join();
+    Con3.join();
 }
+
+/*
+What needs to be accoplished :
+-------------------------------------------------
+Get threads initialized correctly
+Use sleep_until to have consumers to wait until producer produces item as well as sleeping when the queue is full
+Have only one thread run pickItem or AddItem to queue
+
+
+Make sure producer doesn't try to add to full queue and consumer doesen't try to remove from an empty queue
+*/
+
 
 
